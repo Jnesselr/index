5c33e2bc821f038a6b39856da730ba970a0c7375
Justin Nesselrotte <admin@jnesselr.org>
2021-06-11 22:58:55 -0600

    Extracting AppImage automatically

--- pnp/cad/export-3d-files.py
+++ pnp/cad/export-3d-files.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from typing import List
 
 freecad_paths = [
+    '/home/runner/work/index/index/squashfs-root/usr/lib',  # For CI when using AppImage
     '/usr/lib/freecad/lib/',  # For CI
     '/usr/lib/freecad-daily-python3/lib/',  # For Ubuntu
     '/usr/lib64/freecad/lib64/',  # For Fedora
